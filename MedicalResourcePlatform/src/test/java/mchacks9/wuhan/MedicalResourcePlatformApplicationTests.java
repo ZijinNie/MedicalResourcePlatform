00c3abe486024725e05e1e9b55f2f12ef0ee9879
Zijin Nie <nzjjim@gmail.com>
2020-02-01 02:24:10 -0500

    123

---
+++ MedicalResourcePlatform/src/test/java/mchacks9/wuhan/MedicalResourcePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package mchacks9.wuhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalResourcePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
